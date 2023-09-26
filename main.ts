5ffedb45d661ab1b901b334393add475886095d7
TEJ2O-1-2023 <144263858+tej2o-1-2023@users.noreply.github.com>
2023-09-26 12:47:55 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -7,3 +7,20 @@
 
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
+
+// variables
+let lightValue: number
+let lightValuePercentage: number
+
+input.onButtonPressed(Button.A, function() {
+  // input
+  lightValue = input.lightLevel() 
+
+  // process
+  lightValuePercentage = (lightValue / 255) * 100
+  lightValuePercentage = Math.round(lightValuePercentage)
+
+  // output
+  basic.clearScreen()
+  basic.showString('Percentage light is: ' + lightValuePercentage.toString() + '%.')
+})
